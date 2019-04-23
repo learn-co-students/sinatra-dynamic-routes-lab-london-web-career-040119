b4a97419a88d0dbb8a2f34e3e77ac618884fde86
Rebecca Huseyin <rebecca.huseyin.14@ucl.ac.uk>
2019-04-23 17:18:55 +0100

    Add dynamic routes with get

--- app.rb
+++ app.rb
@@ -1,6 +1,44 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
+  get '/reversename/:name' do
+    @user_name = params[:name]
+    "#{@user_name.reverse}"
+  end
+
+  get '/square/:number' do
+    @num = params[:number].to_i
+    squared = @num ** 2
+    "#{squared}"
+  end
+
+  get '/say/:number/:phrase' do
+    @number = params[:number].to_i
+    @phrase = params[:phrase]
+    "#{@phrase * @number}"
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    @word_1 = params[:word1]
+    @word_2 = params[:word2]
+    @word_3 = params[:word3]
+    @word_4 = params[:word4]
+    @word_5 = params[:word5]
+    "#{@word_1} #{@word_2} #{@word_3} #{@word_4} #{@word_5}."
+  end
+
+  get '/:operation/:number1/:number2' do
+    @num_1 = params[:number1].to_i
+    @num_2 = params[:number2].to_i
+    if params[:operation] == "multiply"
+      "#{@num_1 * @num_2}"
+    elsif params[:operation] == "divide"
+      "#{@num_1 / @num_2}"
+    elsif params[:operation] == "add"
+      "#{@num_1 + @num_2}"
+    elsif params[:operation] == "subtract"
+      "#{@num_1 - @num_2}"  
+    end
+  end
 
 end
